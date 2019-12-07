4d28addb0c9c8cddeb60f6995c3c9049ea7b9135
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-07 12:04:30 +0200

    Day 7a

--- solutions/Program.fs
+++ solutions/Program.fs
@@ -5,8 +5,8 @@ open Solutions
 
 [<EntryPoint>]
 let main argv =
-    let path = IO.Path.Combine(__SOURCE_DIRECTORY__, "inputs", "Puzzle06.input")
+    let path = IO.Path.Combine(__SOURCE_DIRECTORY__, "inputs", "Puzzle07.input")
     let input = path |> IO.File.ReadAllLines |> Seq.toList
-    // let input = [ "COM)B"; "B)C"; "C)D"; "D)E"; "E)F"; "B)G"; "G)H"; "D)I"; "E)J"; "J)K"; "K)L" ]
-    printfn "%A" (Puzzle06.solveA input)
+    // let input = [ "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" ]
+    printfn "%A" (Puzzle07.solveA input)
     0 // return an integer exit code
---
+++ solutions/Puzzle07.fs
@@ -0,0 +1,34 @@
+namespace Solutions
+
+open System
+
+module Puzzle07 =
+    let amplify (phase: int) (inputSignal: int) (memory: string) =
+        memory
+        |> Puzzle05.parseOutput [ phase; inputSignal ]
+        |> snd
+        |> List.last
+
+    let trusterSignal (memory: string) (phaseSettings: int list) =
+        phaseSettings |> List.fold (fun output phase -> amplify phase output memory) 0
+
+    let solveA (input: string list) =
+        let memory = input |> List.head
+        let l = [ 0; 1; 2; 3; 4 ] // phase settings
+
+        let combinations =
+            List.collect
+                (fun a ->
+                List.collect
+                    (fun b ->
+                    List.collect (fun c -> List.collect (fun d -> List.map (fun e -> [ a; b; c; d; e ]) l) l) l) l) l
+            |> List.filter (fun l ->
+                l
+                |> Set.ofList
+                |> Set.count = 5)
+        combinations
+        |> List.map (trusterSignal memory)
+        |> List.max
+
+
+    let solveB (input: string list) = 1
