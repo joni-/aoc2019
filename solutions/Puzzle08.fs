0be8e3c949f54830d43b49db97731d524e13f644
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-08 12:38:09 +0200

    Day 8a

---
+++ solutions/Puzzle08.fs
@@ -0,0 +1,63 @@
+namespace Solutions
+
+open System
+
+module Puzzle08 =
+    let createLayer (width: int) (height: int) (digits: int list) =
+        { 0 .. height - 1 }
+        |> Seq.fold (fun (layer, digitsLeft) _ ->
+            let row = digitsLeft |> List.take width
+            let leftOver = digitsLeft |> List.skip width
+            [ row ] |> List.append layer, leftOver) (List.empty, digits)
+
+    let createLayers (width: int) (height: int) (input: string) =
+        let layers = input.Length / (width * height)
+        let createLayer = createLayer width height
+
+        let digits =
+            input.ToCharArray()
+            |> Array.toList
+            |> List.map (string >> int)
+
+        { 0 .. layers - 1 }
+        |> Seq.fold (fun (layers, digitsLeft) _ ->
+            let layer, leftOver = createLayer digitsLeft
+            [ layer ] |> List.append layers, leftOver) (List.empty, digits)
+        |> fst
+
+    let flatten (l: int list list) = l |> List.collect (fun a -> a |> List.map (fun b -> b))
+
+    let countDigits (digit: int) (digits: int list) =
+        let count =
+            digits
+            |> List.countBy id
+            |> Map.ofList
+            |> Map.tryFind digit
+        match count with
+        | Some v -> v
+        | None -> 0
+
+    let countLayerDigits (digit: int) (layer: int list list) =
+        layer
+        |> flatten
+        |> countDigits digit
+
+    let layerWithFewestDigits (digit: int) (layers: int list list list) =
+        let digitCountByLayer = layers |> List.map (countLayerDigits digit)
+        let minDigitCount = digitCountByLayer |> List.min
+        let layerIndexWithFewestDigits = digitCountByLayer |> List.findIndex (fun count -> count = minDigitCount)
+        layers |> List.item layerIndexWithFewestDigits
+
+    let solveA (input: string list) =
+        let layer =
+            input
+            |> List.head
+            |> createLayers 25 6
+            |> layerWithFewestDigits 0
+            |> flatten
+
+        let ones = layer |> countDigits 1
+        let twos = layer |> countDigits 2
+        ones * twos
+
+    let solveB (input: string list) = 1
---
+++ tests/Puzzle08_Tests.fs
@@ -0,0 +1,43 @@
+namespace Tests
+
+open System
+open Xunit
+
+open Solutions
+
+module Puzzle08Test =
+    [<Fact>]
+    let ``Create single layer``() =
+        let expected =
+            [ [ 1; 2; 3 ]
+              [ 4; 5; 6 ] ]
+
+        let layer, leftOver = [ 1; 2; 3; 4; 5; 6; 7 ] |> Puzzle08.createLayer 3 2
+        Assert.Equal<int list list>(expected, layer)
+        Assert.Equal<int list>([ 7 ], leftOver)
+
+    [<Fact>]
+    let ``Converting input into layers``() =
+        let result = "123456789012" |> Puzzle08.createLayers 3 2
+
+        let expectedLayer1 =
+            [ [ 1; 2; 3 ]
+              [ 4; 5; 6 ] ]
+
+        let expectedLayer2 =
+            [ [ 7; 8; 9 ]
+              [ 0; 1; 2 ] ]
+
+        let expected = [ expectedLayer1; expectedLayer2 ]
+
+        Assert.Equal<int list list list>(expected, result)
+
+    [<Fact>]
+    let ``Puzzle08A returns correct result``() =
+        let result = [] |> Puzzle08.solveA
+        Assert.Equal(1, result)
+
+    [<Fact>]
+    let ``Puzzle08B returns correct result``() =
+        let result = [] |> Puzzle08.solveB
+        Assert.Equal(1, result)
