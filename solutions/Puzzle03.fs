596d4c2b55988e1b8e47798a9c1addb3301a2be0
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-08 19:48:24 +0200

    Day 3a

--- solutions/Puzzle03.fs
+++ solutions/Puzzle03.fs
@@ -3,26 +3,77 @@ namespace Solutions
 open System
 
 module Puzzle03 =
-    let parseDirection (item: string) =
+    type Step =
+        { Direction: Direction
+          Amount: int }
+
+    let parseStep (item: string) =
         let direction =
             match item.[0] with
             | 'U' -> UP
             | 'D' -> DOWN
             | 'L' -> LEFT
             | 'R' -> RIGHT
-            | _ -> raise (Exception "Invalid direction")
-        let count = item.Substring(1) |> int
-        direction, count
+            | _ ->
+                sprintf "Invalid direction %A" item
+                |> Exception
+                |> raise
 
-    let appendCoordinates (acc: Coordinate list) (direction: Direction) (amount: int) =
-        let last = List.last acc
-        // TODO: Append coordinates
-        1
+        let amount = item.Substring(1) |> int
+        { Direction = direction
+          Amount = amount }
 
-    let parseCoordinates (input: string list) =
-        [ { x = 0
-            y = 0 } ]
+    let addStep (c: Coordinate) (step: Step) =
+        match step.Direction with
+        | UP ->
+            { 1 .. step.Amount }
+            |> Seq.fold (fun coordinates i -> [ { c with y = c.y + i } ] |> List.append coordinates) List.empty
+        | DOWN ->
+            { 1 .. step.Amount }
+            |> Seq.fold (fun coordinates i -> [ { c with y = c.y - i } ] |> List.append coordinates) List.empty
+        | RIGHT ->
+            { 1 .. step.Amount }
+            |> Seq.fold (fun coordinates i -> [ { c with x = c.x + i } ] |> List.append coordinates) List.empty
+        | LEFT ->
+            { 1 .. step.Amount }
+            |> Seq.fold (fun coordinates i -> [ { c with x = c.x - i } ] |> List.append coordinates) List.empty
 
-    let solveA (input: string list) = 1
+    let parseCoordinates (wire: string) =
+        let steps =
+            wire.Split [| ',' |]
+            |> Array.toList
+            |> List.map parseStep
+
+        steps
+        |> List.fold (fun coordinates step ->
+            let previous = coordinates |> List.last
+            let coords = step |> addStep previous
+            List.append coordinates coords)
+               [ { x = 0
+                   y = 0 } ]
+
+    let solveA (input: string list) =
+        let wire1Coordinates =
+            input
+            |> List.head
+            |> parseCoordinates
+            |> List.skip 1
+            |> Set.ofList
+            |> Set.toList
+
+        let wire2Coordinates =
+            input
+            |> List.tail
+            |> List.head
+            |> parseCoordinates
+            |> List.skip 1
+            |> Set.ofList
+            |> Set.toList
+
+        List.append wire1Coordinates wire2Coordinates
+        |> List.countBy id
+        |> List.filter (fun (_, count) -> count = 2)
+        |> List.map (fun (c, _) -> abs c.x + abs c.y)
+        |> List.min
 
     let solveB (input: string list) = 1
--- tests/Puzzle03_Tests.fs
+++ tests/Puzzle03_Tests.fs
@@ -8,29 +8,28 @@ open Solutions
 module Puzzle03Test =
     [<Fact>]
     let ``Parses coordinates``() =
-        let result = [ "R2"; "U1"; "D1" ] |> Puzzle03.parseCoordinates
-        Assert.Equal
-            ({ x = 0
-               y = 0 }, List.head result)
-        Assert.Equal
-            ({ x = 1
-               y = 0 }, List.item 1 result)
-        Assert.Equal
-            ({ x = 2
-               y = 0 }, List.item 2 result)
-        Assert.Equal
-            ({ x = 2
-               y = 1 }, List.item 3 result)
-        Assert.Equal
-            ({ x = 2
-               y = 0 }, List.item 4 result)
+        let result = "R2,U1,D1" |> Puzzle03.parseCoordinates
+        Assert.Equal<Coordinate list>
+            ([ { x = 0
+                 y = 0 }
+               { x = 1
+                 y = 0 }
+               { x = 2
+                 y = 0 }
+               { x = 2
+                 y = 1 }
+               { x = 2
+                 y = 0 } ], result)
 
     [<Fact>]
     let ``Puzzle03A returns correct result``() =
-        let result = [] |> Puzzle03.solveA
-        Assert.Equal(1, result)
+        Assert.Equal
+            ([ "R75,D30,R83,U83,L12,D49,R71,U7,L72"; "U62,R66,U55,R34,D71,R55,D58,R83" ] |> Puzzle03.solveA, 159)
+        Assert.Equal
+            ([ "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"; "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" ]
+             |> Puzzle03.solveA, 135)
 
-    [<Fact>]
-    let ``Puzzle03B returns correct result``() =
-        let result = [] |> Puzzle03.solveB
-        Assert.Equal(1, result)
+// [<Fact>]
+// let ``Puzzle03B returns correct result``() =
+//     let result = [] |> Puzzle03.solveB
+//     Assert.Equal(1, result)
