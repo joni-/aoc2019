ed0487eda790f05eeaf007eb377df3f5957a55fd
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-09 19:22:30 +0200

    Invoke puzzle solver function dynamically

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My solutions for [AOC 2019](https://adventofcode.com/2019) puzzles in F#.
 
 ```
 # Run solutions/Program.fs:
-dotnet run --project solutions/solutions.fsproj
+dotnet run --project solutions/solutions.fsproj <day#> <part> [input]
 
 # Run specific test module:
 dotnet test --filter FullyQualifiedName~Puzzle01Test
--- solutions/Program.fs
+++ solutions/Program.fs
@@ -1,14 +1,33 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Reflection
 open Solutions
+
+let invokePuzzleSolver (puzzleNumber: string) (part: string) (args: string list) =
+    let asm = Assembly.GetExecutingAssembly()
+    let fn = match asm.GetTypes() |> Array.tryFind (fun t -> t.FullName = sprintf "Solutions.Puzzle%s" puzzleNumber) with
+                | Some t -> t.GetMethods() |> Array.tryFind (fun m -> m.IsStatic && m.Name = sprintf "solve%s" part)
+                | None -> None
+    match fn with
+    | Some method -> method.Invoke(null, [| args |])
+    | None -> "Could not find a solver function to invoke. Check the CLI params" |> Exception |> raise
 
 [<EntryPoint>]
 let main argv =
-    let input = match Array.isEmpty argv with
+    if argv.Length < 2 then
+        "Missing required params: <PuzzleNumber> <Part>" |> Exception |> raise
+
+    let puzzleNumber = argv.[0]
+    let part = argv.[1]
+
+    let input = match argv.Length = 2 with
                 | true ->
-                    let path = IO.Path.Combine(__SOURCE_DIRECTORY__, "inputs", "Puzzle07.input")
+                    let path = IO.Path.Combine(__SOURCE_DIRECTORY__, "inputs", sprintf "Puzzle%s.input" puzzleNumber)
                     path |> IO.File.ReadAllLines |> Seq.toList
-                | false -> argv |> Array.toList
-    printfn "%A" (Puzzle07.solveA input)
+                | false -> argv |> Array.toList |> List.skip 2
+
+    let result = invokePuzzleSolver puzzleNumber part input
+    printfn "%A" result
+
     0 // return an integer exit code
