165d5cd47d2528352898dca722a864406a99ebeb
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-12 21:15:39 +0200

    Day 11b ugly hacking

--- solutions/Puzzle11.fs
+++ solutions/Puzzle11.fs
@@ -14,7 +14,7 @@ module Puzzle11 =
         | LEFT -> { coordinate with x = coordinate.x - amount }
         | RIGHT -> { coordinate with x = coordinate.x + amount }
 
-    let run (initialMemory: int64 array) =
+    let runA (initialMemory: int64 array) =
         let rec helper (memory: int64 array) (extraMemory: int64 array) (index: int) (relativeBase: int)
                 (panelColors: Map<Coordinate, Color>) (instructions: (Coordinate * Color) list) (position: Coordinate)
                 (direction: Direction) =
@@ -85,36 +85,99 @@ module Puzzle11 =
 
         instructions
 
-    // let paintInstructions = List.empty
+    let runB (initialMemory: int64 array) =
+        let rec helper (memory: int64 array) (extraMemory: int64 array) (index: int) (relativeBase: int)
+                (panelColors: Map<Coordinate, Color>) (instructions: (Coordinate * Color) list) (position: Coordinate)
+                (direction: Direction) =
+            let currentPanelColor =
+                match panelColors |> Map.tryFind position with
+                | Some c -> c
+                | None -> BLACK
 
-    // let currentPosition =
-    //     { x = 0
-    //       y = 0 }
+            let inputReader =
+                (fun _ ->
+                if currentPanelColor = BLACK then 0L
+                else 1L)
 
-    // let currentDirection = UP
-    // let currentMemory = fst intcodeResult
+            let intcodeResult = Intcode.parseOutput memory extraMemory index inputReader relativeBase
 
-    // let initialTiles: Map<Coordinate, Color> = Map.empty
-    // initialTiles
+            if List.isEmpty intcodeResult.Outputs then
+                panelColors, instructions
+            else
+                let paintColor =
+                    match intcodeResult.Outputs |> List.head with
+                    | 0L -> BLACK
+                    | 1L -> WHITE
+                    | _ ->
+                        sprintf "Invalid output color: %A" intcodeResult.Outputs
+                        |> Exception
+                        |> raise
+
+                // Paint the current position
+                let newInstructions = [ position, paintColor ] |> List.append instructions
+                let newPanelColors = panelColors |> Map.add position paintColor
+
+                // printfn "Paint %A with %A" position paintColor
+
+                let nextDirection =
+                    match intcodeResult.Outputs
+                          |> List.tail
+                          |> List.head with
+                    | 0L ->
+                        match direction with
+                        | UP -> LEFT
+                        | DOWN -> RIGHT
+                        | LEFT -> DOWN
+                        | RIGHT -> UP
+                    | 1L ->
+                        match direction with
+                        | UP -> RIGHT
+                        | DOWN -> LEFT
+                        | LEFT -> UP
+                        | RIGHT -> DOWN
+                    | _ ->
+                        sprintf "Invalid output turn direction: %A" intcodeResult.Outputs
+                        |> Exception
+                        |> raise
+
+                let nextPosition = move position 1 nextDirection
+                helper intcodeResult.Memory intcodeResult.ExtraMemory intcodeResult.CurrentIndex
+                    intcodeResult.RelativeBase newPanelColors newInstructions nextPosition nextDirection
+
+        let instructions =
+            helper initialMemory (Array.zeroCreate 1000000) 0 0
+                (Map.ofList
+                    [ { x = 0
+                        y = 0 }, WHITE ]) List.empty
+                { x = 0
+                  y = 0 } UP
+
+        instructions
 
     let solveA (input: string list) =
         input
         |> List.head
         |> (fun s -> s.Split [| ',' |])
         |> Array.map int64
-        |> run
+        |> runA
         |> List.map (fun (c, _) -> c)
         |> Set.ofList
         |> Set.count
 
     let solveB (input: string list) =
-        let coordinates =
+        let result =
             input
             |> List.head
             |> (fun s -> s.Split [| ',' |])
             |> Array.map int64
-            |> run
+            |> runB
+
+        let coordinates =
+            result
+            |> snd
             |> List.map (fun (c, _) -> c)
+
+        let colors = result |> fst
 
         let xs = coordinates |> List.map (fun c -> c.x)
         let ys = coordinates |> List.map (fun c -> c.y)
@@ -124,9 +187,23 @@ module Puzzle11 =
         let minY = ys |> List.min
         let maxY = ys |> List.max
 
-        { x = minX ; y = minY}, { x = maxX ; y = maxY }
+        let rows =
+            { minY .. maxY }
+            |> Seq.map (fun row ->
+                { minX .. maxX }
+                |> Seq.map (fun col ->
+                    let coordinate =
+                        { x = col
+                          y = row }
 
+                    let color =
+                        match colors |> Map.tryFind coordinate with
+                        | Some WHITE -> "#"
+                        | Some BLACK -> " "
+                        | None -> " "
 
+                    color)
+                |> (fun seq -> String.Join("", seq)))
 
-
-
+        for r in rows |> Seq.rev do
+            printfn "%A" r
