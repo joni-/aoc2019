bcd71a01ad778a1c2fab9d07b9d4cdb1726942dc
Joni Nevalainen <joninevalainen@gmail.com>
2019-12-22 20:09:13 +0200

    Day 13b slow solution

--- solutions/Puzzle13.fs
+++ solutions/Puzzle13.fs
@@ -29,6 +29,39 @@ module Puzzle13 =
         | Ball -> "*"
         | Score v -> v.ToString()
 
+    let createTile (t: int64 list) =
+        let x = t |> List.head
+
+        let y =
+            t
+            |> List.tail
+            |> List.head
+
+        let numericType = t |> List.last
+
+        if x = -1L && y = 0L then
+            { Coordinate =
+                  { x = int x
+                    y = int y }
+              Type = Score numericType }
+        else
+            let tileType =
+                match List.last t with
+                | 0L -> Empty
+                | 1L -> Wall
+                | 2L -> Block
+                | 3L -> HorizontalPaddle
+                | 4L -> Ball
+                | _ ->
+                    sprintf "Invalid tile type %A" t
+                    |> Exception
+                    |> raise
+            { Coordinate =
+                  { x = int x
+                    y = int y }
+              Type = tileType }
+
+
     let printState (tiles: Tile list) =
         let tilesPerRow =
             tiles
@@ -51,19 +84,56 @@ module Puzzle13 =
             |> List.last = 2L)
         |> List.length
 
+    // Answer was. 10292
     let solveB (input: string list) =
+        let initialBlocks =
+            input
+            |> Shared.simpleIntcodeRun List.empty
+            |> List.chunkBySize 3
+            |> List.map createTile
+            |> List.filter (fun t -> t.Type = Block)
+
+        let mutable blockStatus =
+            initialBlocks
+            |> List.map (fun b -> b.Coordinate)
+            |> Set.ofList
+
         let inputReader (state: Intcode.State) =
-            let numberOfBalls =
+            let tiles =
                 state.Outputs
                 |> List.chunkBySize 3
-                |> List.map (fun t -> t |> List.last)
-                |> List.filter (fun v -> v = 4L)
-                |> List.length
+                |> List.map createTile
 
-            printfn "Reading input... Number of balls: %d" numberOfBalls
+            let lastBallTile =
+                tiles
+                |> List.filter (fun t -> t.Type = Ball)
+                |> List.last
 
-            let choices = [ -1L; 0L; 1L ]
-            choices.[Random().Next(3)]
+            let lastPaddleTile =
+                tiles
+                |> List.filter (fun t -> t.Type = HorizontalPaddle)
+                |> List.last
+
+            let lastScoreTile =
+                tiles
+                |> List.filter (fun t -> t.Coordinate.x = -1)
+                |> List.last
+
+
+            blockStatus <- blockStatus.Remove lastBallTile.Coordinate
+
+
+            if blockStatus.Count % 10 = 0 then
+                printfn "Number of blocks left: %d. Current score: %A" blockStatus.Count lastScoreTile.Type
+
+            // printfn "Number of blocks left: %d" blockStatus.Count
+            // printfn "Reading input... Ball(x=%d), Paddle(x=%d)" lastPaddleTile.Coordinate.x lastBallTile.Coordinate.x
+
+            if lastPaddleTile.Coordinate.x < lastBallTile.Coordinate.x
+            then 1L
+            elif lastPaddleTile.Coordinate.x > lastBallTile.Coordinate.x
+            then -1L
+            else 0L
 
         let initialMemory = input |> Shared.parseIntcodeInput
         Array.set initialMemory 0 2L
@@ -79,39 +149,7 @@ module Puzzle13 =
             Intcode.run initialState inputReader (fun _ -> None)
             |> (fun v -> v.Outputs)
             |> List.chunkBySize 3
-            |> List.map (fun t ->
-                let x = t |> List.head
-
-                let y =
-                    t
-                    |> List.tail
-                    |> List.head
-
-                let numericType = t |> List.last
-
-                if x = -1L && y = 0L then
-                    printfn "Read score tile from %A" t
-                    { Coordinate =
-                          { x = int x
-                            y = int y }
-                      Type = Score numericType }
-                else
-                    let tileType =
-                        match List.last t with
-                        | 0L -> Empty
-                        | 1L -> Wall
-                        | 2L -> Block
-                        | 3L -> HorizontalPaddle
-                        | 4L -> Ball
-                        | _ ->
-                            sprintf "Invalid tile type %A" t
-                            |> Exception
-                            |> raise
-                    { Coordinate =
-                          { x = int x
-                            y = int y }
-                      Type = tileType })
+            |> List.map createTile
 
         stateAfterOneIteration |> printState
         1
-1
